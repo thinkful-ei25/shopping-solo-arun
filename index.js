cba4d20b833e6a7401d1cc62b790167fe2640fdd
Arun Seehra <seehra.a@gmail.com>
2018-09-28 10:14:42 -0500

    Add comments to function stubs
    Not quite pseudocode but it explains the reponsibilities of each function stub.

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 'use strict';
 
+/**
+ * Renders the shopping list to the DOM.
+ */
 function renderShoppingList() {
   console.log('`renderShoppingList` ran');
 }
 
+/**
+ * Sets up event handlers for adding a new item to the shopping list.
+ * Story: "A user should be able to add items to the list."
+ */
 function handleNewItemSubmit() {
   console.log('`handleNewItemSubmit` ran');
 }
 
+/**
+ * Sets up event handlers for checking and unchecking shopping list items.
+ * Story: "A user should be able to check items on the list."
+ */
 function handleItemCheckClicked() {
   console.log('`handleItemCheckClicked` ran');
 }
 
+/**
+ * Sets up events handlers for deleting shopping list items.
+ * Story: "A user should be able to delete items from the list."
+ */
 function handleDeleteItemClicked() {
   console.log('`handleDeleteItemClicked` ran');
 }
