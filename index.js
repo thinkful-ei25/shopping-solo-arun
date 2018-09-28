df53985c1d22c8c1da9cde9a1c8db1d69f13f601
Arun Seehra <seehra.a@gmail.com>
2018-09-28 10:32:34 -0500

    Complete shopping list template
    Currently generateItemElement takes an unused template parameter?

--- index.js
+++ index.js
@@ -14,12 +14,21 @@ const STORE = [
 
 function generateItemElement(item, itemIndex, template) {
   return `
-    <li>${item.name}</li>
-  `;
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
 }
 
 function generateShoppingListItemsString(shoppingList) {
-  console.log('Generating shopping list element');
+  console.log('Generating shopping list elements');
   return shoppingList
     .map(generateItemElement)
     .join('');
